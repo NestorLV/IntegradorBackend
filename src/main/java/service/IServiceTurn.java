f7957417ef6d9c73011c63c47063ccdf8ad30806
Nestor Villalobos <nlvc89@hotmail.com>
2021-09-20 10:31:40 -0500

    se configurar el properties

---
+++ src/main/java/service/IServiceTurn.java
@@ -0,0 +1,8 @@
+package service;
+
+import model.TurnDto;
+
+public interface IServiceTurn extends IServiceCRUD<TurnDto>{
+
+    //TODO pendiente agregar configurar IserviceTurn
+}
---
+++ src/main/java/service/imp/TurnService.java
@@ -0,0 +1,25 @@
+package service.imp;
+
+import model.TurnDto;
+import service.IServiceTurn;
+
+import java.util.List;
+
+public class TurnService implements IServiceTurn {
+
+    //TODO pendiente configurar TurnService
+    @Override
+    public TurnDto save(TurnDto turnDto) {
+        return null;
+    }
+
+    @Override
+    public List<TurnDto> findAll() {
+        return null;
+    }
+
+    @Override
+    public TurnDto update(TurnDto turnDto) {
+        return null;
+    }
+}
