3290e6bc314770dd8551c379839a6f20cd2dec71
Nestor Villalobos <nlvc89@hotmail.com>
2021-09-20 06:41:48 -0500

    Se crea el repo inicial

---
+++ src/test/java/com/example/ExamenFinalBackend/ExamenFinalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExamenFinalBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenFinalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
